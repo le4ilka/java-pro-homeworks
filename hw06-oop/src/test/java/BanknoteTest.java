9521473f668f9d53684130195d92c9496260e716
olga <romanovar-151@ya.ru>
2025-01-15 17:22:37 +0300

    hw06-oop, банкомат finish

--- hw06-oop/src/test/java/BanknoteTest.java
+++ hw06-oop/src/test/java/BanknoteTest.java
@@ -1,11 +1,43 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class BanknoteTest {
-    @DisplayName("First test")
+
+    Set<Banknote> wad = new HashSet<>();
+    Banknote banknote100 = new Banknote(Denomination.HUNDRED, (int) (Math.random() * 10000));
+    Banknote banknote1000 = new Banknote(Denomination.THOUSAND, (int) (Math.random() * 10000));
+    Banknote banknote1001 = new Banknote(Denomination.THOUSAND, (int) (Math.random() * 10000));
+    Banknote banknote101 = new Banknote(Denomination.HUNDRED, (int) (Math.random() * 10000));
+    Banknote banknote5000 = new Banknote(Denomination.FIVE_THOUSAND, (int) (Math.random() * 10000));
+    Banknote banknote10 = new Banknote(Denomination.TEN, (int) (Math.random() * 10000));
+
+    @BeforeEach
+    void createBanknotes() {
+        wad.add(banknote101);
+        wad.add(banknote101);
+        wad.add(banknote100);
+        wad.add(banknote1000);
+        wad.add(banknote1001);
+        wad.add(banknote5000);
+        wad.add(banknote10);
+    }
+
+    @DisplayName("Take banknotes denomination test")
     @Test
-    void test() {
-        System.out.println("Test");
+    void getDenominationTest() {
+        assertThat(banknote10.getDenomination()).isEqualTo(Denomination.TEN);
+    }
+
+    @DisplayName("Take banknotes denomination Value test")
+    @Test
+    void getDenominationValueTest() {
+        assertThat(banknote10.getDenominationValue()).isEqualTo(Denomination.TEN.getDenominationValue());
     }
 
 }
---
+++ hw06-oop/src/test/java/CellTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CellTest {
+
+    @BeforeEach
+    void prepare(){
+
+    }
+    @Test
+    void getBanknotesCountTest(){
+
+    }
+}
